d212cc6c1ce451314df78ae22e70433bf9f195f2
luoxingyue <lyjh123abc@163.com>
2022-09-03 22:50:10 +0800

    删除冲突的依赖

--- onlineJudgebackend/src/test/java/com/oj/onlinejudge/OnlinejudgeApplicationTests.java
+++ onlineJudgebackend/src/test/java/com/oj/onlinejudge/OnlinejudgeApplicationTests.java
@@ -1,21 +1,12 @@
 package com.oj.onlinejudge;
 
-import com.oj.onlinejudge.mapper.UserMapper;
-import com.oj.onlinejudge.pojo.User;
-import com.oj.onlinejudge.service.user.account.RegisterService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class OnlinejudgeApplicationTests {
-    @Autowired
-    private RegisterService registerService;
     @Test
     void contextLoads() {
-        registerService.register("test3", "123456", "123456");
     }
 
 }
