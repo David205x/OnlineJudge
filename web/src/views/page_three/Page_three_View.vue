0042765df2ec08c337c63286743dd94aef3a26fc
Coincidence7 <108170523+Coincidence7@users.noreply.github.com>
2022-09-19 15:12:27 +0800

    main_page

---
+++ web/src/views/page_three/Page_three_View.vue
@@ -0,0 +1,71 @@
+<template>
+  <ContentField>
+    <span>
+      这里是题解界面
+    </span>
+    <div class="row">
+    <div class="col">
+    <div class="card" style="width: 18rem;">
+      <img src="@/assets/1.jpeg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    </div>
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+        <img src="@/assets/1.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+        <img src="@/assets/1.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    </div>
+  </ContentField>
+
+
+
+</template>
+
+<script>
+import ContentField from "@/components/ContentField.vue";
+import { useStore } from "vuex";
+export default{
+  components: { ContentField },
+  setup(){
+
+    const store = useStore();
+    const logged = store.state.user.is_login;
+    store.commit("updatePullingInfo", false);
+    if(store.state.user.is_login){
+      store.dispatch("getinfoInMainPage", {
+        success(){
+          console.log(store.state.user);
+        }
+      })
+    }
+    return {
+      logged,
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
